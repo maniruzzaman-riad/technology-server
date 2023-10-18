ad13d8ce1d08f63aecf37b31a68bc85f794108be
maniruzzaman-riad <monirozzamanr94@gmail.com>
2023-10-19 01:36:28 +0600

    Install express,mongodb,cors and server setup

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 5000;
+
+// midleware........
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('Hello Im runing')
+})
+
+app.listen(port,()=>{
+    console.log(`assignment is runing on port: ${port}`);
+})
